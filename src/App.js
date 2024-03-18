30a273519c2b95e85c4c7071da5ef0b06dde1e2f
Putra1906 <101515568+Putra1906@users.noreply.github.com>
2024-03-18 16:00:09 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navigation from './components/Navigation';
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <div className='App'>
+      <Navigation/>
+    </div>
+  );
+}
+
+export default App;
