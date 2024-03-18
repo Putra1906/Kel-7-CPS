f4792cac6b8a6b8e165c38e8a89227ea26ec6a2b
Kasyif <husainkaasyiful@gmail.com>
2024-03-18 13:25:56 +0700

    perbaikan terakhir

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,73 +1,63 @@
-import { child, get, getDatabase, onValue, ref } from "firebase/database"
+import { onValue, getDatabase, ref } from "firebase/database"
 import FirebaseApp from "../firebase/firebase"
+import { useEffect, useState } from "react"
 
 const database = ref(getDatabase(FirebaseApp))
 
-let data= ""
-await get(child(database,"/")).then((snapshot) => {
-    if (snapshot.exists()) {
-        data = JSON.stringify(snapshot.val())
-    }
-})
+export default function Home({data}) {
+	const [datain, setData] = useState("{}")
+	const [larik, setLarik] = useState([])
 
-const ardatain = JSON.parse(data)
-const waktu = Object.keys(ardatain)
+	useEffect(() => {
+		return onValue(database, (snapshot) => {
+			const data0 = JSON.stringify(snapshot.val())
 
-function Table() {
-    const kunci = Object.getOwnPropertyNames(ardatain)
-    const keadaan = Object.values(ardatain)
-    let larik = []
-    for (let i = 0; i < kunci.length; i++) {
-        larik[i] = keadaan[i]
-        larik[i].waktu = kunci[i].slice(2)
-    }
+			if (snapshot.exists()) {
+				setData(data0)
+				const ardatain = JSON.parse(datain)
+				const kunci = Object.getOwnPropertyNames(ardatain)
+				const keadaan = Object.values(ardatain)
+				let gabungan = []
 
-    const daftar = larik.map(nilai =>
-        <tbody key={nilai["waktu"]}>
-            <tr>
-                <th scope="row">{nilai["waktu"]}</th>
-                <td>{nilai["cahaya"]}</td>
-                <td>{nilai["hujan"]}</td>
-            </tr>
-        </tbody>
-    );
+				for (let i = 0; i < kunci.length; i++) {
+					gabungan[i] = keadaan[i]
+					gabungan[i].waktu = kunci[i].slice(2)
+				}
+				setLarik(gabungan)			
+			}
+		})
+	})
 
-    return (
-        <>
-            {daftar}
-        </>
-    )
+	const daftar = larik.map(nilai =>
+		<tbody key={nilai.waktu}>
+			<tr>
+				<th scope="row">{nilai.waktu}</th>
+				<td>{nilai.cahaya}</td>
+				<td>{nilai.hujan}</td>
+			</tr>
+		</tbody>
+	);
+
+	return (
+		<>
+			<header><div className="pricing-header p-3 pb-md-4 mx-auto text-center"><h1 className="display-4 fw-normal text-body-emphasis">Monitor Output Perangkat</h1><p className="fs-5 text-body-secondary">Perangkat IoT ini memonitor cahaya dan tingkat hujan (data mentah dari RainDrop Sensor) yang berguna untuk memelihara tanaman.</p></div></header>
+
+			<div className="container my-5">
+				<div className=" p-5 text-center bg-body-tertiary rounded-3">
+					<h1 className="text-body-emphasis">Tabel Monitoring</h1>
+					<table className="table">
+						<thead>
+							<tr>
+								<th scope="col">T+</th>
+								<th scope="col">Cahaya</th>
+								<th scope="col">Tingkat Hujan</th>
+							</tr>
+						</thead>
+						{daftar}
+					</table>
+				</div>
+			</div>
+		</>
+	)
 }
 
-export default function Home() {
-    let databaru = ""
-
-    onValue(database, (snapshot) => {
-        if (snapshot.exists()) {
-            databaru = JSON.stringify(snapshot.val())
-        }
-    })
-
-
-    return (
-        <>
-            <header><div className="pricing-header p-3 pb-md-4 mx-auto text-center"><h1 className="display-4 fw-normal text-body-emphasis">Monitor Output Perangkat</h1><p className="fs-5 text-body-secondary">Perangkat IoT ini memonitor cahaya dan tingkat hujan (data mentah dari RainDrop Sensor) yang berguna untuk memelihara tanaman.</p></div></header>
-
-            <div className="container my-5">
-                <div className=" p-5 text-center bg-body-tertiary rounded-3">
-                    <h1 className="text-body-emphasis">Tabel Monitoring</h1>
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">T+</th>
-                                <th scope="col">Cahaya</th>
-                                <th scope="col">Tingkat Hujan</th>
-                            </tr>
-                        </thead>
-                        <Table />
-                    </table>
-                </div>
-            </div>
-        </>
-    )
-}
