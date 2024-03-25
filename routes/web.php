e445b5d7a75b68fea443c5305e60c9fd2583981c
Putra1906 <101515568+Putra1906@users.noreply.github.com>
2024-03-25 15:21:24 +0700

    Routes Login Register

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomAuthController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/login',[CustomAuthController::class,'login']);
+Route::get('/registration',[CustomAuthController::class,'registration']);
